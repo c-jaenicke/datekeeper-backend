23b0153c5adcbcad0dfcc06c78ae4e6ddb8a1847
c-jaenicke <me@chrisj.de>
2023-04-04 11:30:25 +0200

    Initial Commit

---
+++ src/main/java/datekeeper/datekeeperbackend/DatekeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package datekeeper.datekeeperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatekeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatekeeperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/datekeeper/datekeeperbackend/DatekeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package datekeeper.datekeeperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatekeeperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
